430314320109b19b9a8d77e625fd3ebf60d8b09f
user <you@example.com>
2022-12-06 01:05:15 +0300

    Optimized Task5

--- src/Task5.java
+++ src/Task5.java
@@ -1,19 +1,13 @@
 public class Task5 {
     public static void main(String[] args) {
         int[][] array = Task4.createAndPrintDoubleArray(5, 5);
-
+        System.out.println();
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; ; ) {
-                System.out.println(array[i][j]);
-                break;
-            }
+            System.out.printf("%3d",array[i][i]);
         }
-        System.out.println("----------------------");
+        System.out.println();
         for (int i = 0; i < array.length; i++) {
-            for (int j = array.length - 1 - i; ; ) {
-                System.out.println(array[i][j]);
-                break;
-            }
+            System.out.printf("%3d", array[i][array.length - 1 - i]);
         }
     }
 }
